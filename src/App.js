ca7f7b7885a673c7379879ad3de0bfd376999872
Rafan Syed <rafansyed@Rafans-MBP-2.home>
2022-09-25 16:18:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect }from 'react';
+import axios from 'axios'
 import './App.css';
+import Header from './components/ui/Header';
+import Search from './components/ui/Search';
+import CharacterGrid from './components/characters/CharacterGrid';
 
-function App() {
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsloading] = useState(true)
+  const [query, setQuery] = useState('')
+ 
+useEffect(() => {
+  const fetchItems = async () => {
+    const result = await axios.default.get(`https://gogoanime.herokuapp.com/search?keyw=${query}`)
+    setItems(result.data)
+    //console.log(items)
+    setIsloading(false)
+
+  }
+    fetchItems();
+}, [query])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header />
+     <Search getQuery= {(q) => setQuery(q)} />
+     <CharacterGrid isLoading={isLoading} items ={items} /> 
     </div>
   );
-}
+
+
+  }
 
 export default App;
---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CharacterItem = ({ item }) => { 
+  return (
+    <div className='card'>
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img src={item.animeImg} alt='' />
+        </div>
+        <div className='card-back'>
+          <h1>{item.animeTitle}</h1>
+          <ul>
+            <li>
+              <strong>Title:</strong> {item.animeTitle}
+            </li>
+            <li>
+              <strong><a href={item.animeUrl}>Where To Watch</a></strong> 
+            </li>
+            <li>
+              <strong>Status:</strong> {item.status}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CharacterItem
